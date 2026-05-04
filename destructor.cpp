5bb8fa059279c70715320584e08f77c3265904a2
Thaddea4 <adeyasyie@gmail.com>
2026-05-04 17:46:46 +0700

    menambahkan program utama

--- destructor.cpp
+++ destructor.cpp
@@ -46,3 +46,13 @@ void angka::isiData()
     }
     cout << endl;
 }
+
+int main()
+{
+    angka belajarcpp(3);                 // Constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); // Constructor dipanggil
+
+    delete ptrBelajarcpp; // Destructor dipanggil
+
+    return 0;
+}
